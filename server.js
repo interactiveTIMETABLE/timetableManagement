fe74dba709c7bc548acb2f31df0f062481105641
AnkanMondal <ankanmondal45@gmail.com>
2023-03-16 10:00:11 +0530

    Server files

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const fs = require('fs')
+//require("./db/connect")
+
+app.use('/public', express.static('public'))
+
+app.use(express.json())
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.get('/', (req, res) => {
+    res.statuscode = 200
+    const data = fs.readFileSync('index.html', 'utf8')
+    res.end(data.toString())
+})
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+})
