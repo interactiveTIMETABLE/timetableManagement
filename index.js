03054364d49723e49b7f34ddff30c9eb1cad0842
AnkanMondal <ankanmondal45@gmail.com>
2023-03-17 11:18:18 +0530

    Changes to CSS and default option added

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function populate(department_select, semester_select) {
         var optionArray = ['sem1|Sem 1', 'sem2|Sem 2', 'sem3|Sem 3', 'sem4|Sem 4', 'sem5|Sem 5', 'sem6|Sem 6', 'sem7|Sem 7', 'sem8|Sem 8']
     } else if (dSel.value == "d4") {
         var optionArray = ['sem1|Sem 1', 'sem2|Sem 2', 'sem3|Sem 3', 'sem4|Sem 4']
+    } else {
+        var optionArray = ['select_semester|Select Semester']
     }
     for (var option in optionArray) {
         var pair = optionArray[option].split("|")
