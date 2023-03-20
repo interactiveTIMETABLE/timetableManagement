dc65c6d58488af881deadec10b42702b7e9117a5
AnkanMondal <ankanmondal45@gmail.com>
2023-03-20 10:14:42 +0530

    Established connection to controller

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const fs = require('fs')
 const route = express.Router()
+const controller = require('./controller')
 
 route.get('/', (req, res) => {
     res.statuscode = 200
     const data = fs.readFileSync('index.html', 'utf8')
     res.end(data.toString())
 })
+
+route.post('/api/dept_db', controller.create)
 
 module.exports = route
